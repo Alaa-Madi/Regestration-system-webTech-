d9db44acc318931fa223bb4be55528c3afa20017
yafa-jawabreh21 <130245877+yafa-jawabreh21@users.noreply.github.com>
2023-05-09 09:22:27 +0300

    Update db_tables.py

--- db_tables.py
+++ db_tables.py
@@ -1,18 +1,21 @@
 db.define_table('courses',
-Field('code','string'),
-Field('name','string'),
+Field('code','string',required=True, notnull=True,requires=IS_NOT_EMPTY()),
+Field('name','string',requires=IS_NOT_EMPTY()),
 Field('description','string'),
-Field('prerequisites','string'),
+Field('prerequisites','string','reference courses',
+		requires=IS_IN_DB(db, 'courses.code', '%(name)s')),
 Field('instructor','string'),
-Field('capacity','integer'),
-Field('scheduled','integer'))
+Field('capacity','integer',requires=IS_INT_IN_RANGE(0,120)),
+Field('scheduled','integer','reference coursesschedules', requires=IS_IN_DB(db, 'coursesschedules.id', '%(days)s: %(startTime)s - %(endTime)s'),primarykey=['code']),
+Field('id','integer')
+)
 
 db.define_table('coursesschedules', 
 Field('id','integer'),
-Field('days','string'),
-Field('startTime','time'),
-Field('endTime', 'time'),
-Field('RoomNo','string')
+Field('days','string',requires=IS_NOT_EMPTY()),
+Field('startTime','time',requires=IS_NOT_EMPTY()),
+Field('endTime', 'time',requires=IS_NOT_EMPTY()),
+Field('RoomNo','string',requires=IS_NOT_EMPTY())
 )
 
 db.define_table('studentschedules',
@@ -43,5 +46,4 @@ Field('registration_key','string'),
 Field('reset_password_key','string'),
 Field('registration_id','string'),
 )
-
 
